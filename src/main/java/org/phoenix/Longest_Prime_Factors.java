e586284329878dac41a60411af848f6b12b37109
Phoenix <anmolkhanna2003@gmail.com>
2024-02-20 08:20:29 +0530

    First 3 problems, SOLVED!

--- src/main/java/org/phoenix/Longest_Prime_Factors.java
+++ src/main/java/org/phoenix/Longest_Prime_Factors.java
@@ -2,8 +2,9 @@ package org.phoenix;
 import java.util.ArrayList;
 public class Longest_Prime_Factors {
     public static void main(String...args){
-        System.out.println(primes(600_851_475_143L)); //6857
-        //System.out.println(isPrime(775121));
+        System.out.println(primes(13195L)); //6857
+        System.out.println(primes(600851475143L));
+        System.out.println(isPrime(35));
     }
     public static int primes(long n){
         int currentResult = 1;
@@ -15,7 +16,7 @@ public class Longest_Prime_Factors {
     }
     public static boolean isPrime(int n){
         int sqrt = (int)Math.sqrt(n);
-        for(int i = 2; i < sqrt; i++){
+        for(int i = 2; i <= sqrt; i++){
             if(n%i==0)return false;
         }
         return true;
